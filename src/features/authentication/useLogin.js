d696b2a73375637becb472f8bb6e83054028c8f2
Amr Waheed <amrmohamed877@gmail.com>
2025-02-10 23:14:04 +0200

    implementing the user login and authentication with protection

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+    const queryclient = useQueryClient()
+    const navigate = useNavigate()
+    const { mutate: login, isPending: islogin } = useMutation({
+        mutationFn: ({ email, password }) => loginApi({ email, password }),
+        onSuccess: (user) => {
+            queryclient.setQueryData(['user'], user.user)
+            toast.success('login successfully')
+            navigate('/dashboard')
+        },
+        onError: (err) => {
+            toast.error('email or password are not correct ')
+            console.log("ERROR", err)
+        }
+    })
+    return { login, islogin }
+}

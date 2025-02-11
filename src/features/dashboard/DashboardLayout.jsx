69d16c85b1c226d3a70745dad0576b2ec41a9934
Amr Waheed <amrmohamed877@gmail.com>
2025-02-11 22:09:57 +0200

    finishing part of dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,9 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
+import {useCabins} from "../cabins/useCabins";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -7,4 +12,31 @@ const StyledDashboardLayout = styled.div`
   gap: 2.4rem;
 `;
 
+function DashboardLayout() {
+  const { isPending: isRecentBookings, recentBookings } = useRecentBookings();
 
+  const {
+    isPending: isRecentStays,
+    stays,
+    confirmedStays,
+    numDays,
+  } = useRecentStays();
+
+  const { cabins, isPending: isCabins } = useCabins();
+
+  if (isRecentBookings || isRecentStays || isCabins) return <Spinner />;
+  return (
+    <StyledDashboardLayout>
+      <Stats
+        bookings={recentBookings}
+        confirmedStays={confirmedStays}
+        numDays={numDays}
+        cabinCount={cabins.length}
+      />
+      <div>Todays activity</div>
+      <div>Chart Stay Durations</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
